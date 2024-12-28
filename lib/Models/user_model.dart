acf5a35017f9575458ece1ada26ac4a097bb11a9
Tanishq <tanishq162005@gmail.com>
2024-12-28 16:14:29 +0530

    added

--- lib/Models/user_model.dart
+++ lib/Models/user_model.dart
@@ -1,13 +1,33 @@
 class UserModel {
   final String name;
-  final String mobile;
   final String email;
-  final Map<String, dynamic> contacts;
+  final String mobile;
+  Map<String, dynamic> contacts;
+  bool isSafe;
 
   UserModel({
     required this.name,
-    required this.mobile,
     required this.email,
-    required this.contacts,
+    required this.mobile,
+    this.contacts = const {},
+    this.isSafe = true,
   });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      mobile: json['mobile'] ?? '',
+      contacts: json['contacts'] ?? {},
+      isSafe: json['isSafe'] ?? true,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'email': email,
+        'mobile': mobile,
+        'contacts': contacts,
+        'isSafe': isSafe,
+      };
 }
